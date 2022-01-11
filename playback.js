33654664dc47f79033e7514dbf09972553d52a95
Ravana <47528708+ravana69@users.noreply.github.com>
2022-01-11 11:49:41 +0530

    Update playback.js

--- playback.js
+++ playback.js
@@ -1,47 +1,11 @@
 const sources = [
-  "http://rd.shalombeatsradio.com:8006/stream", // Shalom Beats
-  "http://88.198.138.88:8000/jesusradio/;.m3u", // Jesus Radio Malayalam
-  "http://199.195.194.140:8012/stream", // Sehion Radio
-  "http://5.79.88.132:7084/;stream.mp3", // Psalms Radio
-  "https://dreamsiteradiocp2.com/proxy/rmindia1?mp=/stream", // Radio Maria
-  "https://ephphatha.radioca.st/stream?type=http&nocache=15206", // Ephphatha
-  "https://s2.radio.co/sf61790a26/listen", // Malayalam Christian Network
-  "https://stream.radio.co/s1383afdc9/listen?ver=52115", // Shalom
-  "http://live.jesuscomingfm.com:8132/;", // Jesus Coming FM
-  "http://5.79.88.132:7233/;type=mp3", // Gloria Radio Malayalam
-  "https://stream.radio.co/s9015a8e16/listen", // Radio Wind
-  "https://listen.radioking.com/radio/378295/stream/428998", // Tharangam Radio
-  "https://ice31.securenetsystems.net/DVN", // DVN Radio
-  "http://66.55.145.43:7704/stream/1/", // Malayalam English Christian Radio
-  "https://gains.reviveradio.net/proxy/rafaradio?mp=/stream", // Rafa Radio
-  "http://radio.zionmediait.com:5327/;", // Kripa FM Bhilai
-  "https://gains.reviveradio.net/proxy/menorah?mp=/stream", // Menorah FM
-  "https://stream.radio.co/sf2c714555/listen", // Old Fashioned Christian Music Live
-  "https://servidor15.brlogic.com:7602/live", // ILR (INDIA LIVE RADIO) Ernakulam
-  "http://37.187.93.104:8586/stream" // Blessings Radio
+  "http://stream.zeno.fm/2pcxz945dchvv", // Beethoven
+  "http://stream.zeno.fm/3qmnn155dchvv" // Mozart
 ];
 
 const labels = [
-  [ "Shalom Beats", "http://rd.shalombeatsradio.com:8006/stream"],
-  [ "Jesus Radio Malayalam", "http://88.198.138.88:8000/jesusradio/;.m3u"],
-  [ "Sehion Radio", "http://199.195.194.140:8012/stream"],
-  [ "Psalms Radio", "http://5.79.88.132:7084/;stream.mp3"],
-  [ "Radio Maria", "https://dreamsiteradiocp2.com/proxy/rmindia1?mp=/stream"],
-  [ "Ephphatha", "https://ephphatha.radioca.st/stream?type=http&nocache=15206"],
-  [ "Malayalam Christian Network", "https://s2.radio.co/sf61790a26/listen"],
-  [ "Shalom", "https://stream.radio.co/s1383afdc9/listen?ver=52115"],
-  [ "Jesus Coming FM", "http://live.jesuscomingfm.com:8132/;"],
-  [ "Gloria Radio Malayalam", "http://5.79.88.132:7233/;type=mp3"],
-  [ "Radio Wind", "https://stream.radio.co/s9015a8e16/listen"],
-  [ "Tharangam Radio", "https://listen.radioking.com/radio/378295/stream/428998"],
-  [ "DVN Radio", "https://ice31.securenetsystems.net/DVN"],
-  [ "Malayalam English Christian Radio", "http://66.55.145.43:7704/stream/1/"],
-  [ "Rafa Radio", "https://gains.reviveradio.net/proxy/rafaradio?mp=/stream"],
-  [ "Kripa FM Bhilai", "http://radio.zionmediait.com:5327/;"],
-  [ "Menorah FM", "https://gains.reviveradio.net/proxy/menorah?mp=/stream"],
-  [ "Old Fashioned Christian Music Live", "https://stream.radio.co/sf2c714555/listen"],
-  [ "ILR (INDIA LIVE RADIO) Ernakulam", "https://servidor15.brlogic.com:7602/live"],
-  [ "Blessings Radio", "http://37.187.93.104:8586/stream"]
+  [ "Shalom Beats", "http://stream.zeno.fm/2pcxz945dchvv"],
+  [ "Mozart", "http://stream.zeno.fm/3qmnn155dchvv"]
 ];
 
 let playingIndex = 0; // current radio
